a763eba6d72a3db5f1b682822bfb7aa07b847742
TLmmie <52090464+standbyme-ge@users.noreply.github.com>
2020-06-03 16:57:42 +0800

    Update project-2.m

--- project-2.m
+++ project-2.m
@@ -20,3 +20,45 @@ T（x）= 255(x/ 255)^γ          （11）
 2.使用（1）计算g（x）。
 3.根据（3）变换到频域并获得高频量度F。
 4.应用阈值测试以确定对比度增强是否发生。
+
+
+%单图测试
+%[fn,pn]=uigetfile('*tif');
+%I=imread([pn,fn]);
+%figure,subplot(2,1,1);imshow(I);
+%subplot(2,1,2),imhist(I);
+%imwrite(I_CE,strcat('对比度0.9-',fn));%单文件保存测试
+%figure,subplot(2,1,1); imshow(I_CE);title('0.9'); 
+%subplot(2,1,2),imhist(I_CE);  %显示对比增强处理后的图与直方图
+
+clc,clear,close all
+
+%%%对比度增强处理%%%
+
+%1.导入图像集
+FileList=dir('C:\Users\Administrator\Desktop\contrast-enhance\*tif');
+[FM,FN] = size(FileList);
+
+%2.循环体
+for Fi=1:FM
+
+%2.1 预处理
+
+imx = strcat('C:\Users\Administrator\Desktop\contrast-enhance\',FileList(Fi).name);
+I=im2double(imread(imx));
+I1=I;
+I2=I;
+
+%2.2 对比度增强处理
+
+X = 0.9;  %γ取值0.9 or 1.2
+I_CE1=255*(I1/255).^X;  %幂定律变换
+
+X = 1.2;  
+I_CE2=255*(I2/255).^X;  %幂定律变换
+
+imwrite(I_CE1,strcat('对比度0.9-',FileList(Fi).name));  %改名存储
+imwrite(I_CE2,strcat('对比度1.2-',FileList(Fi).name));  %改名存储
+end
+
+%%%篡改检测%%%
