4968ebacc42f9ced987d3c4a193b1036739f8606
TLmmie <52090464+standbyme-ge@users.noreply.github.com>
2020-06-04 01:41:05 +0800

    Update project-2.m

--- project-2.m
+++ project-2.m
@@ -3,17 +3,6 @@
 
 T（x）= 255(x/ 255)^γ          （11）
 
-更改每个图像，并且γ值介于0.5到2.0之间。 
-另外，每个图像都受到图3左上方显示的映射的影响，
-该映射旨在在每个图像的最亮和最暗区域中展现细节。
-然后将这些更改后的图像与原始图像集组合，以形成4092张图像的测试数据库。 
-所提出的检测算法用于确定测试数据库中的每个图像是否都经过某种形式的对比度增强。
- 在该模拟中，选择β（ω）为（4）中指定的形式，
-其中截止参数参数c =7π/ 8。
-通过分别计算:
-正确分类的已更改图像的百分比
-和错误分类的未更改图像的百分比，
-可以确定给定阈值η的检测概率Pd和错误警报Pfa。 然后，将这些概率用于构建一系列接收器工作特性（ROC）曲线。
 (2):
 所提出的对比度增强检测算法可以概括如下：
 1.获得图像的直方图h（x）。
@@ -22,7 +11,7 @@ T（x）= 255(x/ 255)^γ          （11）
 4.应用阈值测试以确定对比度增强是否发生。
 
 
-%单图测试
+%单图测试部分代码
 %[fn,pn]=uigetfile('*tif');
 %I=imread([pn,fn]);
 %figure,subplot(2,1,1);imshow(I);
@@ -33,7 +22,7 @@ T（x）= 255(x/ 255)^γ          （11）
 
 clc,clear,close all
 
-%%%对比度增强处理%%%
+%%% 一、对比度增强处理 %%%
 
 %1.导入图像集
 FileList=dir('C:\Users\Administrator\Desktop\contrast-enhance\*tif');
@@ -46,55 +35,68 @@ for Fi=1:FM
 
 imx = strcat('C:\Users\Administrator\Desktop\contrast-enhance\',FileList(Fi).name);
 I=im2double(imread(imx));
-I1=I;
 I2=I;
 
 %2.2 对比度增强处理
 
-X = 0.9;  %γ取值0.9 or 1.2:发现：γ小于1 将会变得更亮，大于1则会更加暗。
-I_CE1=(255*(I1/255)).^X;  %幂定律变换
-
-X = 1.2;  
+X = 2;  % γ=0.6时 threshold=8.1 刚好均分。 γ=2时，threshold=9 原图多一张
 I_CE2=(255*(I2/255)).^X;  %幂定律变换
 
-imwrite(I_CE1,strcat('对比度0.9-',FileList(Fi).name));  %改名存储
-imwrite(I_CE2,strcat('对比度1.2-',FileList(Fi).name));  %改名存储
+imwrite(I_CE2,strcat('对比度2-',FileList(Fi).name));  %改名存储
 end
 
-%%%篡改检测%%%
-%%
-[fn,pn]=uigetfile('*tif');
-I=imread([pn,fn]);
-[Iy,Ix]=size(I);
-figure,subplot(2,1,1);imshow(I);
-subplot(2,1,2),imhist(I);
-%%
-h=imhist(I);
-[y,x]=size(h);
-p=zeros(y,x);
-for i=1:y
-if(h(i)<=4)
-    p(i)=0.5-0.5*cos((h(i)*pi)/4);
+%%% 二、篡改检测%%%
+
+clc,clear,close all
+
+%1.载图+阈值设置
+
+FileList=dir('C:\Users\Administrator\Desktop\contrast-enhance\*tif');
+[FM,FN] = size(FileList);
+threshold=9; %γ=0.6时 threshold=8.1 刚好均分。 γ=2时，threshold=9 原图多一张
+I_original=0;
+I_enhance=0;
+
+%2.循环体
+
+for Fi=1:FM
+
+imx = strcat('C:\Users\Administrator\Desktop\contrast-enhance\',FileList(Fi).name);
+I=im2double(imread(imx));
+
+%2.1 公式（1） g(x)=p(x)*h(x)
+
+[Iy,Ix]=size(I); %为N提供图像总像素点
+[h,x]=imhist(I); %获取x的直方图h(x)
+[m,n]=size(h);  %获取直方图横纵向量数
+p=zeros(m,n);  %p(x)预设空间
+for i=1:m  %公式（2）p(x)相关的不等式
+if(i<=4)  %NP宽为4个像素
+    p(i)=0.5-0.5*cos((i*pi)/4);
 else
-    if(h(i)>=252)
-        p(i)=0.5+0.5*cos((pi*(h(i)-252))/4);
+    if(i>=252)
+        p(i)=0.5+0.5*cos((pi*(i-252))/4);
     else
         p(i)=1;
     end
 end
 end
-g=p.*h;
-%%
-N=Iy*Ix;
-G=fft(g);
-BT=zeros(y,x);
-C=pi*7/8;
-for i=1:y
-    if(abs(G(i))>=C)
-        BT(i)=1;
-    else
-        BT(i)=0;
-    end
+g=p.*h;  %公式（1）
+
+%2.2 公式（3）F=（1/N）*sum(abs(BT(w)*G(w)))
+N=Iy*Ix;  %N为像素总数
+G=fft(g); %傅里叶变换
+BT=zeros(m,n);  %β（w）属于0~1的加权函数
+C=pi*7/8;  %论文给定先验值C
+BT=(abs(G)>C); %公式（4）不等式
+F=(1/N)*sum(abs(BT.*G));  %公式（3） 加权度量F
+if(F>threshold)  %判断F与阈值大小以断定是否进行对比度增强。
+    I_enhance=I_enhance+1;
+else
+    I_original=I_origin+1;
 end
-F=(1/N)*abs(BT.*G);
-figure,plot(F);
+end
+I_original,I_enhance
+
+Pd=I_enhance/50  %正确分类已篡改个数
+Pfa=abs(50-I_original)/50  %不正确分类未篡改个数
